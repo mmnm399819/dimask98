e5851ec69d98a83b1720c7dbd494c1fdb5c22c43
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2019-11-01 13:59:52 +0300

    Create flharab.js

---
+++ flharab.js
@@ -0,0 +1,61 @@
+$('.nav-tabs').addClass('fl').css('width','100%');
+	$('.light.break').css('color','#595B61')
+	$('.nav-tabs>li>a').css({"background-image":"url(https://chatshams.com/sico/z1d2n7i5vl10.jpeg)"});
+	$('.s1.fa.fa-user.label.badgex.label-as-badge.label-success').css('color','#EDEDED')
+	$('.loginstat').css('color','#A9ADB8')
+	$('.uzr.fl.corner.borderg.mm ').css('sold','#8A8D96')
+	$('.s1.fa.fa-user.label.badgex.label-as-badge.label-success ').css('background-color','#A9ADB8')
+	$(`<style> .fitimg.fl.u-pic { box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(243, 238, 238, 0.767); border-radius: 50px; } </style>`).insertBefore('body');
+	$(`<style>
+	.fitimg.fl.u-pic {
+	box-shadow: inset 0 30px 30px rgba(0,0,0,.075), 0 0 8px rgb(243, 238, 238, 0.767);
+	border-radius","16px 16px 16px 0px;
+	}
+	</style>`).insertBefore('body')
+	$(`<link rel="stylesheet" type="text/css" href="=-light,">
+	<link rel="stylesheet" href="">
+	<style>
+	.border {box-#: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(80, 79, 81);border: 10px color: #507A9C ;}
+	
+	.lonline.light.break{background-image: url();background-size: inherit;border-top:5px groove #A9ADB8;padding-top:5px}
+	.sultan111,div#d2{background-repeat: no-repeat;background-size: contain;background-image: url();background-color:#ffffff}a.label.label-primary.fl{height:35px;border-radius:19px 19px 0 0!important;padding:8px!important;border:2px solid #595B61}
+	}
+	.fitimg.fl.u-pic {box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgb(80, 79, 81);border-radius: 75px 0px 75px 75px;}
+	ul.nav.nav-tabs.fl li a {color: #7F9BB3; box-#: inset 0 2px 2px rgb(80, 79, 81), 0 0 8px #(80, 79, 81);font-family: '',;font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+	ul.nav.nav-tabs.fl li {border-bottom: 3px rgb(80, 79, 81);width: 33.3%;}
+	.label-primary, .btn-primary {border-radius: 14px 14px 14px 14px;}
+	
+	label.label.tc.border.btn.fl {border-radius: 24px 0px 24px 0px;}
+	.label.tc.border.btn.fl, .label.tc.border.btn.fl {border: 3px solid #4D0808;font-family: 'jazeera',FontAwesome;border-radius: 5px 5px 5px 0px;}
+	label.fl.nosel.label.pnhead {font-family: '',;width: 80%;text-align: -webkit-center;background-color: #E8E8E8;color: #C0C4D1;border-radius: 24px 24px 24px 0px;};
+	</style>`).insertBefore('body');
+	$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '1px solid #A6A6A6',borderLeft: '1px solid #A6A6A6',borderTop: '1px solid #A6A6A6',borderBottom: '1px solid #A6A6A6',paddingTop: '1px',borderRadius: '10px'});
+	$('.uzr.fl.corner.borderg').css({'margin': '0px 0',borderRight: '3px solid #adadad',borderLeft: '3px solid #adadad',borderTop: '.40px solid #adadad',borderBottom: '.50px solid #adadad',paddingTop: '3px',borderRadius: '3px 3px 3px 3px'});
+	$('.nav-tabs>li>a').css('color','#ffffff')
+	$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera">
+	<link rel="stylesheet" href="https://rawgit.com/muaml1/ahs0/master/%D9%81%D9%84%D8%A9%20%D8%A7%D9%84%D8%AE%D9%84%D9%8A%D8%AC.css">
+	</style>`).insertBefore('body');
+	$('.u-msg.break.fl').css('color','#000000')
+	
+	$(`<center><div width="99.5%" style="background-image:url(https://chatshams.com/sico/z1d2n7i5vl10.jpeg);color: #595B61;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ffffff;border-top: 2px solid#ffffff;padding-bottom: 2px!important;"><font style="background-color: #fff;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;">مرحبـاً بكـم جميعاً في شـات عشقي </font></div><center/>`).insertBefore('#d2');
+	$(`<div style="width:100%;" id="" class="btn btn-primary btn-ali" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">إضغـط هُنآ </div>
+	<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"> </p>إدآرة ألمـؤقع / خالد السوري </p> <i class="fa fa-"></i> <a href="" style="border-radius: 90px 90px 90px 90px;width:48%;margin:2px;" class="fl mini fa fa-star
+	btn btn-primary"> تمييز </a>
+	<a href="" style="border-radius: 90px 90px 90px 90px;width:49%;margin:2px;p dir="ltr" align="center" class="fl mini fa fa-heart
+	btn btn-primary"> </a>
+	‏ <center></div></div></div>`).insertAfter('.nav-tabs');
+	
+	$(`<button style="width:24px;margin:2px;" style="margin-top:2px;margin-left:2px;" class="fa fa-microphone fl btn btn-info"></button>`).insertBefore('button.fr.fa.fa-share-alt.sndfile.fl.btn.btn-primary');
+	$("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star'></span> لـوحة تحكم / شآت عشقي") 
+	$('.label.border.btn.label-danger.tc.fl').css('background-color','#A9ADB8')
+	$('.label.border.btn.label-danger.tc.fl').css('color','#ffffff')
+	$('.label.fl.nosel.label.pnhead').css({"background-image":"url(https://chatshams.com/sico/z1d2n7i5vl10.jpeg)"});
+	$('.room.borderg.hand.nosel.fl').css({"background-image":"url(https://chatshams.com/sico/)"});
+	$('.room.borderg.hand.nosel.fl')
+
+
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>دمـشقي </span></a>-->
+<a href="https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >تصميم :  آلہـَدمـشۣۗہـقۗـۙيـہ 🕊❥⌁ للتواصل اضغط هنا/a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+setTimeout (function (){  $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تعديل و تصميم: <a href='https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/'>  المهيب الدمشقي © 2019</a></div>").insertAfter($(".loginstat"));  },1000);
