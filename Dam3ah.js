67b64564fb1ca1d88d12452a6b4d7604e3e30b94
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2019-12-07 23:26:33 +0300

    Create Dam3ah.js

---
+++ Dam3ah.js
@@ -0,0 +1,45 @@
+$(`<link rel="stylesheet" type="text/css" href="=-light,">
+<link rel="stylesheet" href="">
+<style>
+
+
+div#l1 input#u1 {border-radius: 15px 15px 0px 0px;border: 1px solid #FFE0F6;width: 99%;margin-top: 3px;margin-left:1px;text-align: center;font-family: 'jazeera-light',FontAwesome;}
+div#l1 button {border-radius: 0px 0px 15px 15px;width: 99%;height: 30px;}
+div#l3 button {border-radius: 15px;width: 99%;height: 30px;margin-left:1px;}
+div#l2 button {border-radius: 15px;width: 85%;height: 30px;}
+div#l2 input#pass1, div#l2 input#u2, div#l3 input#pass2, div#l3 input#u3{width: 49%;padding: 0px!important;float: right;margin: 2px 5px 2px 0px;;text-align: center;font-family: 'jazeera-light',FontAwesome;}
+div#l2 input#u2, div#l3 input#u3{margin-left: -6px;border-radius: 0px 15px 15px 0px;border: 1px solid #949494;}
+div#l2 input#pass1, div#l3 input#pass2 {border-radius: 15px 0px 0px 15px;border: 1px solid #949494;}
+div#l2 , div#l3 {padding: 2px!important;}
+div#l1 {height: 80px;margin: 0px 1px -18px 1px;padding: 0px!important;}
+div#l3 br , div#l2 br {display: none;}
+
+.s1.fa.fa-user.label.badgex.label-as-badge.label-success {background-color: #808080!important;}
+.fr.borderg, .fr.borderg {border: 1px solid #949494;font-family: 'jazeera',FontAwesome;border-radius: 15px 0px 15px 0px;}
+
+
+ul.nav.nav-tabs.fl li a:hover {background-color: #6E1D1D;color: white;}
+ul.nav.nav-tabs.fl li {width: 33.3%;border-top: 2px solid #6E1D1D;border-radius: 15px 15px 0px 0px;background-color: #FFFFFF;border-bottom: 3px solid #000000;}
+ul.nav.nav-tabs.fl li a {font-family: 'jazeera',FontAwesome;font-size: 13px!important;padding: 6px!important;text-align: center!important;border-radius: 15px 15px 0px 0px;width: 100%;color: #949494;}
+.sultan111,div#d2{background-repeat: no-repeat;background-size: contain;background-image: url();background-color:#ffffff}a.label.label-primary.fl{height:35px;border-radius:19px 19px 0 0!important;padding:8px!important;border:2px solid #298FC2}
+}
+.element.style {
+text-align: left;
+background-color: white;
+width: 99%;
+padding: 2px 1px 1px;
+border-radius: 10px;
+margin: 1px 0px;
+border-width: 1px 2px;
+border-style: solid;
+border-color: rgba(130, 124, 124) rgb(130, 124, 124);
+}
+img.fitimg.fl.u-pic {
+border: 2px solid #520918;border-radius:19px 19px 19px 0px;
+border-radius: 19px;
+.fitimg.fl.u-pic {box-shadow: inset 0 1px 1px rgba(113, 105, 105), 0 0 8px rgb(113, 105, 105);border-radius: 75px 0px 75px 75px;}
+
+.well {font-family: 'jazeera',FontAwesome;font-size: 16px!important;padding: 3px!important;text-align: center!important;width: color: #000000;white-space: nowrap;border-radius: 15px 15px 15px 15px;}
+</style>`).insertBefore('body');
+
+$["getScript"]("https://raw.githack.com/mmnm399819/dimaski98/master/Recording.js")
