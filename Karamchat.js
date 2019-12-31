1e12c275be086a46c9a33dc82472394b24750274
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2019-12-31 15:37:49 +0300

    Create Karamchat.js

---
+++ Karamchat.js
@@ -0,0 +1,15 @@
+
+eval((emo+'').replace('[0-9][0-9]|','[0-9][0-9][0-9]|[0-9][0-9]|'));
+$(`<link rel="stylesheet" type="text/css" href=""><style>.des {position: absolute;transform:matrix(1, 0, 0, 1, 86, -156);margin-top: 2px;width: 300px;height: 60px;float: left;background: transparent url(https://www.te3b.com/sico/z1dp69r9uc10.png) no-repeat scroll right top;text-align: left;}button.btn.fr.btn-success.fa.fa-refresh {display: none;}a.label.label-primary.fl img.fl {display: none;}a.label.label-primary.fl {border-radius: 3px!important;margin: 2px -156px;padding: 5px!important;margin-bottom: 2px;text-align: right!important;}#des22 {text-align: center!important;font-size: 100%!important;}label.fl.label.loginstat {background-color: transparent!important;position: absolute;top: 0;margin-top: 4px;left: 0;}.fr.borderg, .fr.borderg a {font-size: 10px !important;padding: 0 5px!important;}span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {padding: 0px 3px 2px;margin-right: 5px;}label.label.label-primary.mini.fl {width: 64%!important;padding: 1px;border-radius: 3px!important;}</style>`).insertBefore('body');$($(".urkick")[0].outerHTML.replace('user-times','star').replace('urkick','umod').replace('darkorchid','green')).text('ترقيه الى مراقب').insertAfter('.urkick');$(".nonot").remove();$.getScript("https://rawcdn.githack.com/waleed-qaid/des/fb5ecfb0a41265477c14936b5f3e7d2d8ab2598b/www.1des.net- te3b-color.js")
+
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="" class="fl" style="margin-right: 2px; width: 25px;">');
+$(`<img src="/sico/z1dt700cm810.jpg" class="fr" style="width:100%;margin-top: 0px;"> 
+<a href="https://chatsham.com" style="border-radius: 3px 12px 11px 11px;width:100px;margin:3px;" class="fl mini fa fa-heartbeat btn btn-primary"> اعادة تحميل </a>
+<a href="/sico/" style="border-radius: 3px 12px 11px 11px;width:100px;margin:3px;" class="fl mini fa fa-graduation-cap btn btn-primary"> القوانين </a>
+<div style="border-radius: 3px 12px 11px 11px;width:100px;margin:3px;" class="fa mini fa-phone btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> اضغط هنا </div><div class="collapse" id="collapseExample"><center><div class="well"><font color="#FF0000"> ادارة الموقع / مهند ، خيول ، دمشقي #</font> </BR>_________ </BR><font color="#0000FF"> لتغيير اللون اضغط على أحد القلوب </font> </BR><img src="/sico/z1dn82c50m10.gif"> x</BR><img src="/sico/z1dn82c50m10.gif">  شات كرم الشام للجوال </BR> `).insertBefore('.nav-tabs');
+
+$('a[href="https://jawalhost.com/"]').removeAttr('href');
+$("[data-target=#wall]").attr('title','يمنع السوالف فالحائط')
+$('.nav-tabs').addClass('fl').css('width','100%');
+$(".checkbox label").html(`<input id="stealth" type="checkbox" value="">`);
+eval((close+'').replace('location.reload()','location.href="/"'));
