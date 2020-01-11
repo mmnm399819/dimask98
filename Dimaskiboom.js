15e192f2ce1743d86d844f1ff23c4976501a8cff
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-01-11 11:28:55 +0300

    Update Dimaskiboom.js

--- Dimaskiboom.js
+++ Dimaskiboom.js
@@ -63,10 +63,4 @@ $('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid 
 
 
 
-$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;">Ø§Ù…ÙŠØ± Ø¯ÙˆÙ†ÙŠØªÙŠ</span></a>-->
-<a href="amir donity" target="_blank" style="width:107px; margin-right: 5px;
-margin-left: 2px;text-align: center;float: right;border-radius: 5px;
-border-color: #990033;
-padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >Style By:amir donity</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
 
- 
