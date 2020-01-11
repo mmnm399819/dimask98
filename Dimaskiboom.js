c9c5e81e998f7d5331688f6d4b55276003940de4
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-01-11 11:07:03 +0300

    Create Dimaskiboom.js

---
+++ Dimaskiboom.js
@@ -0,0 +1,72 @@
+'use strict';
+var _0xd3e6 = [".nav-tabs", "insertBefore", '<form name="amir">\r\n<div class="amirdonity">\r\n\t\t\t\t\t<map id="ColorsMap" name="ColorsMap">\r\n\t\t\t\t\t<area alt="" coords="275, 0, 298, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\r\n\t\t\t\t\t<area alt="" coords="244, 0, 268, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\r\n\t\t\t\t\t<area alt="" coords="213, 0, 238, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\r\n\t\t\t\t\t<area alt="" coords="183, 0, 207, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\r\n\t\t\t\t\t<area alt="" coords="152, 0, 177, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\r\n\t\t\t\t\t</map>\r\n\t\t\t <img alt="" height="40" src="https://amirdonity23.000webhostapp.com/donity/amir.gif" width="300" usemap="#ColorsMap"></div>\r\n</form>\r\n\r\n',
+"https://amirdonity23.000webhostapp.com/donity/amirtof3.js", "getScript", "body", '<link rel="stylesheet" type="text/css" href="">\r\n<link rel="stylesheet" href="">\r\n<link href="https://amirdonity23.000webhostapp.com/donity/amir1.css" rel="stylesheet" type="text/css" />\r\n<link href="https://amirdonity23.000webhostapp.com/donity/amir2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\r\n<link href="https://amirdonity23.000webhostapp.com/donity/amir3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\r\n<link href="https://amirdonity23.000webhostapp.com/donity/amir4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\r\n<link href="https://amirdonity23.000webhostapp.com/donity/amir5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\r\n<style>\r\n</style>\r\n'];
+$(_0xd3e6[2])[_0xd3e6[1]](_0xd3e6[0]);
+$[_0xd3e6[4]](_0xd3e6[3]);
+$(_0xd3e6[6])[_0xd3e6[1]](_0xd3e6[5]);
+
+
+
+$('.nav-tabs').addClass('fl').css('width','100%'); 
+$(`<link rel="stylesheet" type="text/css" href="">
+<style>
+.amirdonity {
+position: absolute;transform:matrix(1, 0, 0, 1, 86, 0);
+margin-top: 2px;
+width: 300px;
+height: 60px;
+float: left;
+background: transparent url(https://amirdonity23.000webhostapp.com/donity/z1dp69r9uc10.png) no-repeat scroll right top;
+text-align: left;}
+button.btn.fr.btn-success.fa.fa-refresh {
+display: none;}
+a.label.label-primary.fl img.fl {
+display: none;}
+a.label.label-primary.fl {
+border-radius: 3px;
+margin: 2px -156px;
+padding: 5px!important;
+margin-bottom: 2px;text-align: right!important;}
+
+label.fl.label.loginstat {
+background-color: transparent!important;
+position: absolute;
+top: 0;
+margin-top: 4px;left: 0;}
+.fr.borderg, .fr.borderg a {
+font-size: 8px !important;
+padding: 0 5px!important;}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success {
+padding: 0px 3px 2px;
+margin-right: 5px;}
+label.label.label-primary.mini.fl {
+width: 60%!important;
+padding: 1px;
+border-radius: 3px!important;}
+a.btn.minix.fa.fa-comment.fr {background-color: #2b3e52!important;float: left;padding: 0px 4px 4px;color: #ffffff!important;}a.btn.minix.fa.fa-thumbs-up.fr {background-color: #2b3e52!important;margin-right: 50px;padding: 0px 4px 4px;color: #ffffff!important;}a.btn.minix.fa.fa-times.fr{background-color: #2b3e52!important;padding: 0px 4px 4px;color: #ffffff!important;}
+textarea.form-control.borderg.primaryborder.corner.fr.u-topic {
+    font-size: 16px!important;
+}
+textarea.form-control.borderg.primaryborder.corner.fr.ulikeins {
+    font-size: 16px!important;
+}
+</style>`).insertBefore('body');
+$(".u-pic").css("border-radius","24px");
+$('.room.borderg.hand.nosel.fl').css({'margin': '1px 0',borderRight: '3px solid #480032',borderLeft: '3px solid #480032',borderTop: '1px solid #480032',borderBottom: '1px solid #480032',paddingTop: '3px',borderRadius: '5px'});
+
+
+
+
+
+
+
+
+
+
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;">Ø§Ù…ÙŠØ± Ø¯ÙˆÙ†ÙŠØªÙŠ</span></a>-->
+<a href="amir donity" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+border-color: #990033;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >Style By:amir donity</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+ 
