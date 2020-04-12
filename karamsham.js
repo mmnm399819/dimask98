27152a91e5c55b6d90f8c135a8be026abdd8617e
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-04-12 20:32:12 +0300

    Update karamsham.js

--- karamsham.js
+++ karamsham.js
@@ -3,9 +3,3 @@ $[_0x9cb3[1]](_0x9cb3[0])
 $(`<img src="sico/z1e5b1d8le10.gif" class="fr" style="width:100%;margin-top: 1px;">
 `).insertBefore('.nav-tabs');
 
-$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>دمـشقي </span></a>-->
-<a href="https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/" target="_blank" style="width:107px; margin-right: 5px;
-margin-left: 2px;text-align: center;float: right;border-radius: 5px;
-padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >تصميم :  آلہـَدمـشۣۗہـقۗـۙيـہ 🕊❥⌁ للتواصل اضغط هنا/a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
-setTimeout (function (){  $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تعديل و تصميم: <a href='https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/'>  محمد الدمشقي © 2020</a></div>").insertAfter($(".loginstat"));  },1000);
-
