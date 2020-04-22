b82446a885aaa287b469e60621ca69185d838bd6
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-04-23 02:22:07 +0300

    Update Likesreq.js

--- Likesreq.js
+++ Likesreq.js
@@ -1,13 +1,13 @@
 Tsend_=Tsend;
-reqlikes=10;
+reqlikes=100;
 Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}}
 
 sendpic_=sendpic;
-reqlikes=10;
+reqlikes=100;
 sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}}
 
 setprofile_=setprofile;
-reqlikes=10;
+reqlikes=100;
 setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
 
 $["getScript"]("https://raw.githack.com/mmnm399819/dimaski98/master/Recording.js")
