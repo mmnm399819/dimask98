7ccea7b34e61fe47a876aed1fd166ff4254b5513
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-11 07:58:36 +0300

    Update Likesreq.js

--- Likesreq.js
+++ Likesreq.js
@@ -37,3 +37,8 @@ $(".fa-gear").text("آعدآدت الشات").css("width","");
 setTimeout (function (){  $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تعديل و تصميم: <a href='https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/'>  محمد الدمشقي © 2020</a></div>").insertAfter($(".loginstat"));  },1000);
 
 $("div#tlogins input").attr("maxlength",5),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+
+$(`<!--<a class="fl fa fa-image btn ui-corner-all ui-shadow ui-btn ui-btn-inline borderg" style="color:purple;margin:2px;width: 106px;text-align: center;font-size:15px!important;"><span dir="rtl"> (0) </span><span style="font-size: 15px!important;>دمـشقي </span></a>-->
+<a href="https://api.whatsapp.com/send?phone=905349315009&amp;text=&amp;source=&amp;data=/" target="_blank" style="width:107px; margin-right: 5px;
+margin-left: 2px;text-align: center;float: right;border-radius: 5px;
+padding: 3px;font-family: 'Cairo', sans-serif, Fontawesome;font-size: 13px!important;" class="SLPBeats fab fa-angellist" >تصميم :  آلہـَدمـشۣۗہـقۗـۙيـہ 🕊❥⌁ للتواصل اضغط هنا/a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
