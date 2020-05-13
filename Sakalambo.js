6098a7497e7853d1ca3bdad20b67daa1852fe124
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-13 03:35:29 +0300

    Create Sakalambo.js

---
+++ Sakalambo.js
@@ -0,0 +1,44 @@
+Tsend_=Tsend;
+
+reqlikes=100;
+
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+
+reqlikes=100;
+
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+
+reqlikes=100;
+
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
+
+$(`<img src="sico/z1e5b1d8le10.gif" class="fr" style="width:100%;margin-top: 0px;">
+
+<a id="des3" href="chatsham.com" style="border-radius: 24px 0px 24px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-
+
+ btn btn-primary"> ♻ تحديث</a>
+
+<a id="des3" href="chatsham.com" style="border-radius: 0px 24px 0px 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star
+
+ btn btn-default"> تميز- مَعنآ </a>
+
+<a id="des3" href="chatcham.com" style="border-radius: 24px 0px 24px 0px;width:26%;margin:2px;" class="fl mini  fa fa-image
+
+btn btn-primary"> رمزيات </a>
+
+<a id="des3" href="chatsham.com" style="border-radius: 0px 24px 0px 24px;width:24%;margin:2px;" class="fl mini  fa fa-paint-brush
+
+btn btn-default"> مدونتنا </a>
+
+`).insertBefore('.nav-tabs');
+
+$.getScript("https://cdn.statically.io/gh/mmnm399819/dimask98/master/Chatshamm.js");
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+
+$(`<link rel="stylesheet" type="text/css" href=""><link rel="stylesheet" href=""><style>img.fl.ustat {display: none!important;}</style>`).insertBefore('body');
+
