c07a7bc18ad33ba6f0aa7aa96e3b606d0afacc6a
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-16 16:28:34 +0300

    Create Shamchat.js

---
+++ Shamchat.js
@@ -0,0 +1,47 @@
+Tsend_=Tsend;
+
+reqlikes=100;
+
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+
+reqlikes=100;
+
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+
+reqlikes=100;
+
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
+
+$(`<img src="sico/z1e5b1d8le10.gif" class="fr" style="width:100%;margin-top: 0px;">
+<a id="des3" href="chatsham.com" style="border-radius: 24px 0px 24px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-
+ btn btn-primary"> ♻ تحديث</a>
+<a id="des3" href="chatsham.com" style="border-radius: 0px 24px 0px 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star
+ btn btn-default"> تميز- مَعنآ </a>
+<a id="des3" href="chatcham.com" style="border-radius: 24px 0px 24px 0px;width:26%;margin:2px;" class="fl mini  fa fa-image
+btn btn-primary"> رمزيات </a>
+<a id="des3" href="chatsham.com" style="border-radius: 0px 24px 0px 24px;width:24%;margin:2px;" class="fl mini  fa fa-paint-brush
+btn btn-default"> مدونتنا </a>
+`).insertBefore('.nav-tabs');
+var _0x3311 = [".nav-tabs", "insertBefore", "<form name=\"jump\">\x0D\x0A<div class=\"MyColors\">\x0D\x0A\x09\x09\x09\x09\x09<map id=\"ColorsMap\" name=\"ColorsMap\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"270, 0, 295, 25\" href=\"javascript:chooseStyle(\'none\',60)\" shape=\"rect\" title=\"1\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"239, 0, 265, 25\" href=\"javascript:chooseStyle(\'Co2\',60)\" shape=\"rect\" title=\"2\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"209, 0, 235, 25\" href=\"javascript:chooseStyle(\'Co3\',60)\" shape=\"rect\" title=\"3\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"178, 0, 203, 25\" href=\"javascript:chooseStyle(\'Co4\',60)\" shape=\"rect\" title=\"4\">\x0D\x0A\x09\x09\x09\x09\x09<area alt=\"\" coords=\"148, 0, 173, 25\" href=\"javascript:chooseStyle(\'Co5\',60)\" shape=\"rect\" title=\"5\">\x0D\x0A\x09\x09\x09\x09\x09</map>\x0D\x0A\x09\x09\x09        <img alt=\"\" height=\"40\" src=\"https://1.top4top.net/p_1388gqx1c1.gif\" width=\"300\" usemap=\"#ColorsMap\"></div>\x0D\x0A</form>\x0D\x0A\x0D\x0A\x0D\x0A   \x0D\x0A\x0D\x0A", "https://cdn.statically.io/gh/mmnm399819/dimask98/master/Sham1.js", "getScript", ".loginstat", "insertAfter", "<div class=\'fr borderg\' style=\'padding:2px;background-color:white;float:left;margin-left:4px;\'> تصميم :<a href=\'https://api.whatsapp.com/send?phone=905349315009&text=&source=&data\'>محمد دمشقي</a></div>", "border-radius", "3px", "css", ".u-pic", "1px 0", "2px", "5px", ".hand.nosel.fl.uzr.border", "0px 0", ".uzr.fl.corner.borderg", "<input id=\"stealth\" type=\"checkbox\" value=\"\" maxlength=\"12\"><i class=\"fa fa-eye-slash\" aria-hidden=\"true\">", "html", ".checkbox label", "width", "100%", "fl", "addClass", "body", "<link rel=\"stylesheet\" type=\"text/css\" href=\"\">\x0D\x0A\x0D\x0A\x0D\x0A<link href=\"https://cdn.statically.io/gh/mmnm399819/dimask98/master/Sham8.css\" rel=\"stylesheet\" type=\"text/css\" />\x0D\x0A<link href=\"https://cdn.statically.io/gh/mmnm399819/dimask98/master/Sham3.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co2\" type=\"text/css\" />\x0D\x0A<link href=\"https://cdn.statically.io/gh/mmnm399819/dimask98/master/Sham4.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co3\" type=\"text/css\" />\x0D\x0A<link href=\"https://cdn.statically.io/gh/mmnm399819/dimask98/master/Sham5.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co4\" type=\"text/css\" />\x0D\x0A<link href=\"https://cdn.statically.io/gh/mmnm399819/dimask98/master/Sham6.css\" media=\"screen\" rel=\"alternate stylesheet\" title=\"Co5\" type=\"text/css\" />\x0D\x0A\x0D\x0A<style>\x0D\x0A\x0D\x0A.MyColors {\x0D\x0A    position: absolute;\x0D\x0A    transform: matrix(1, 0, 0, 1, 72, -2);\x0D\x0A    margin-top: 2px;\x0D\x0A    width: 300px;\x0D\x0A    height: 60px;\x0D\x0A    float: right;\x0D\x0A    background: transparent url(https://cdn.statically.io/gh/mmnm399819/dimask98/master/z1e62dbf4u10.png) no-repeat scroll right top;\x0D\x0A    text-align: left;\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A#des0 {\x0D\x0A    background-color: #3c3c3c;\x0D\x0A\x09border-radius: 15px 15px 0px 0!important;\x0D\x0A}\x0D\x0A\x0D\x0Aa.fa.fa-user-plus {\x0D\x0A    color: #ff0000;\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0Alabel.fl.nosel.label.pnhead {\x0D\x0Abackground-image: -webkit-linear-gradient(45deg,hsla(0,0%,100%,.2) 25%,transparent 0,transparent 50%,hsla(0,0%,100%,.2) 0,hsla(0,0%,100%,.2) 75%,transparent 0,transparent)!important;\x0D\x0A\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A#des3 {\x0D\x0A    margin: 1px!important;\x0D\x0A}\x0D\x0A\x0D\x0Aul.nav.nav-tabs.fl {\x0D\x0A    margin-top: 1px!important;\x0D\x0A}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A</style>\x0D\x0A"];
+$(`${_0x3311[2]}`)[_0x3311[1]](_0x3311[0]);
+$[_0x3311[4]](_0x3311[3]);
+$(_0x3311[7])[_0x3311[6]]($(_0x3311[5]));
+$(_0x3311[11])[_0x3311[10]](_0x3311[8], _0x3311[9]);
+$(_0x3311[15])[_0x3311[10]]({
+    'margin': _0x3311[12],
+    paddingTop: _0x3311[13],
+    borderRadius: _0x3311[14]
+});
+$(_0x3311[17])[_0x3311[10]]({
+    'margin': _0x3311[16],
+    paddingTop: _0x3311[13],
+    borderRadius: _0x3311[14]
+});
+$(_0x3311[20])[_0x3311[19]](`${_0x3311[18]}`);
+$(_0x3311[0])[_0x3311[24]](_0x3311[23])[_0x3311[10]](_0x3311[21], _0x3311[22]);
+$(_0x3311[0])[_0x3311[24]](_0x3311[23])[_0x3311[10]](_0x3311[21], _0x3311[22]);
+$(`${_0x3311[26]}`)[_0x3311[1]](_0x3311[25])
