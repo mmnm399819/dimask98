5caf68d2eb4bb824300d2648218eb06092e4537f
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-16 03:47:17 +0300

    Create Kimooo.js

---
+++ Kimooo.js
@@ -0,0 +1,17 @@
+$('.fl.ustat')
+
+.css('width', '3px'), $('.fa.fa-user-plus')
+
+.html(`❥ تسجيل عضوية `), document.getElementById('u1')
+
+.placeholder = 'اكتب أي اسم من 8 احرف فقط ', 
+
+document.getElementById('pass1')
+
+.placeholder = 'كلمة المرور', document.getElementById('u2')
+
+.placeholder = 'فخآمة طلتك تنورنا', document.getElementById('pass2')
+
+.placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+
+.placeholder = '♥ أكتب أي اسم من 8 أحرف فقط';
