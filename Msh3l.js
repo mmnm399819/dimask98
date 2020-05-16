249fcf50337bf8311efbd097c6e258f36870ddf7
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-16 18:13:04 +0300

    Update Msh3l.js

--- Msh3l.js
+++ Msh3l.js
@@ -1,4 +1,15 @@
-Tsend_=Tsend; reqlikes=100; Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يا اهلا وسهلا بك في شات دمعه للجوال لتسطيع الكتابة يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}} sendpic_=sendpic; reqlikes=100; sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يا اهلا وسهلا بك في شات دمعه للجوال لتسطيع الكتابة يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}} setprofile_=setprofile; reqlikes=100; setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يا اهلا وسهلا بك في شات دمعه للجوال لتسطيع الكتابة يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
+ 
+Tsend_=Tsend;
+reqlikes=100;
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('حياكم في شات دمعه للجوال لتستطيع الكتابة يتطلب منك الحصول على 100 لايك '+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+reqlikes=100;
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+reqlikes=100;
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
 
 
 $("div#tlogins input").attr("maxlength",8),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
