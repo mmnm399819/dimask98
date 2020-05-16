3e766c0304845d0de202c9c74aeede31233cdfdd
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-16 18:17:34 +0300

    Update Msh3l.js

--- Msh3l.js
+++ Msh3l.js
@@ -13,5 +13,22 @@ setprofile=function(){if(getuser(myid).rep<reqlikes){alert (' حياكم في ش
 
 
 $("div#tlogins input").attr("maxlength",8),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+$('.fl.ustat')
+
+.css('width', '3px'), $('.fa.fa-user-plus')
+
+.html(`❥ تسجيل عضوية `), document.getElementById('u1')
+
+.placeholder = 'اكتب أي اسم من 8 احرف فقط ', 
+
+document.getElementById('pass1')
+
+.placeholder = 'كلمة المرور', document.getElementById('u2')
+
+.placeholder = 'فخآمة طلتك تنورنا', document.getElementById('pass2')
+
+.placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3')
+
+.placeholder = '♥ أكتب أي اسم من 8 أحرف فقط';
 
 
