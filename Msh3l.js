c2073b8e278553d5ff4502504e4302273a7f05f6
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-05-16 17:52:56 +0300

    Create Msh3l.js

---
+++ Msh3l.js
@@ -0,0 +1,7 @@
+Tsend_=Tsend; reqlikes=100; Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يا اهلا وسهلا بك في شات دمعه للجوال لتسطيع الكتابة يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}} sendpic_=sendpic; reqlikes=100; sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يا اهلا وسهلا بك في شات دمعه للجوال لتسطيع الكتابة يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}} setprofile_=setprofile; reqlikes=100; setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يا اهلا وسهلا بك في شات دمعه للجوال لتسطيع الكتابة يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
+
+
+$("div#tlogins input").attr("maxlength",8),$("textarea#tbox.fl.corner.tbox.filw").attr("maxlength",1000);
+
+
+$('.fl.ustat') .css('width', '3px'), $('.fa.fa-user-plus') .html(`❥ تسجيل عضوية `), document.getElementById('u1') .placeholder = 'اكتب أي اسم من 8 احرف فقط ', document.getElementById('pass1') .placeholder = 'كلمة المرور', document.getElementById('u2') .placeholder = 'فخآمة طلتك تنورنا', document.getElementById('pass2') .placeholder = ' ㋡ بـاسوردك ولاتنساه يـ جميل ', document.getElementById('u3') .placeholder = '♥ أكتب أي اسم من 8 أحرف فقط'; $("label.label.tc.border.cp.btn.label-danger.fl").html("<span class='fl fa fa-star-half-o'></span> لـوحة الشـآت ") $(".fa-gear").text("آعدآدت ").css("width","");
