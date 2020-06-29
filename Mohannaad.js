e46a1ad98f9dd6d9fbdddddb3c726399dbcf7bce
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-06-29 20:57:37 +0300

    Update and rename Mohannad.js to Mohannaad.js

--- Mohannaad.js
+++ Mohannaad.js
@@ -1,1 +1,3 @@
 $(`<a href="https://api.whatsapp.com/send?phone=966553491881&text=&source=&data" target="_blank" style=" font-family: 'El Messiri', sans-serif;width:107px;color: #942942;text-decoration:none; margin-right: 5px; margin-left: 2px;text-align: center;float: right;border-radius: 5px; border-color: #942942; padding: 3px;" class="border" ><i class="fa fa-heartbeat"></i> للتواصل مع مهند</a>`).insertBefore('a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg');
+
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>صاحب الموقع: <a href='https://api.whatsapp.com/send?phone=966553491881&text=&source=&data'>مهند السوري</a></div>").insertAfter($(".loginstat"));  },1000);
