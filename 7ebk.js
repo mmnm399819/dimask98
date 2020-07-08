40a726f62bba811add7f9566d839aeef53733c6b
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2020-07-08 19:25:13 +0300

    Create 7ebk.js

---
+++ 7ebk.js
@@ -0,0 +1,44 @@
+Tsend_=Tsend;
+
+reqlikes=100;
+
+Tsend=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي'); return;}else{Tsend_();}}
+
+sendpic_=sendpic;
+
+reqlikes=100;
+
+sendpic=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات '+reqlikes+' برعاية محمد الدمشقي'); return;}else{sendpic_();}}
+
+setprofile_=setprofile;
+
+reqlikes=100;
+
+setprofile=function(){if(getuser(myid).rep<reqlikes){alert('يتطلب منك الحصول على لايكات'+reqlikes+' برعاية محمد الدمشقي '); return;}else{setprofile_();}}
+
+
+$(`<iframe width="390" height="140" src="https://www.youtube.com/embed/WsKZcHOpcFM" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+
+<marquee direction="right" width="100%"; 
+direction="right" style="margin-bottom: 0px;font-family: 'Changa', sans-serif; height: 20px;color: #0A1BFF;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5" style="height: 20px; width: 100%;"
+<a id="des3" href="chatsham.com" style="border-radius: 24px 0px 24px 0px;width:23%;margin:2px;" class="fl mini  fa  fa-
+
+ btn btn-primary"> ♻ تحديث</a>
+
+<a id="des3" href="chatsham.com" style="border-radius: 0px 24px 0px 24px;width:23%;margin:2px;" class="fl mini  fa   fa-star
+
+ btn btn-default"> تميز- مَعنآ </a>
+
+<a id="des3" href="chatcham.com" style="border-radius: 24px 0px 24px 0px;width:26%;margin:2px;" class="fl mini  fa fa-image
+
+btn btn-primary"> رمزيات </a>
+
+<a id="des3" href="chatsham.com" style="border-radius: 0px 24px 0px 24px;width:24%;margin:2px;" class="fl mini  fa fa-paint-brush
+
+btn btn-default"> مدونتنا </a>
+
+`).insertBefore('.nav-tabs');
+
+$.getScript("https://cdn.statically.io/gh/mmnm399819/dimask98/master/Dimashkiteest.js");
+
+
