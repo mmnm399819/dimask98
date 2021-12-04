3d8f99b192ed12d22f87a022b46ae7e056f1dac9
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2021-12-04 21:20:10 +0300

    Create Nono.js

---
+++ Nono.js
@@ -0,0 +1,93 @@
+var _0xa8e6 = ['https://raw.githack.com/alshanfara/style/main/idafat.js', 'getScript', 'width', '', 'css', 'text', '.ae.fa.label.label-primary.fa-microphone', 'div#tlogins .lonline.light.break', 'insertBefore', `<style>.hand.corner{background-color:#000!important;border:2px solid #E1CC3A!important;color:#E1CC3A;border-radius:0;text-align:center;border-style:solid;border-right:7px solid!important;border-left:7px solid!important;border-color:#2ea1ad;border-left-style:double!important;border-right-style:double!important}div.break.fl::before{content:" ʷʷʷ.ˢᵃʰʳ⁰.ᶰᵉᵗ";color:#EFD100!important;font-family:'jazeera',FontAwesome;font-size:16px}.break.fl{text-align:center;padding:5px!important;font-family:'jazeera',FontAwesome;color:red;-webkit-animation:mymove .5s infinite;animation:mymove .5s infinite}</style>`, `<center><div id="asim1"><a id="asim7"rel="dofollow" title="القوانين" href=""style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+
+\
+
+<a id="asim7"rel="dofollow" title="الاشتراكات" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+
+\
+
+<a id="asim7"rel="dofollow" title="راديو" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+
+\
+
+<a id="asim7"rel="dofollow" title="زخرفة اسماء" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a><a id="asim7"rel="dofollow" title="اتصل بنا" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-envelope btn btn-primary" target="_blank"></a>
+
+\
+
+<a id="asim7"rel="dofollow" title="قوانين السوابر" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+
+\
+
+<a id="asim7"rel="dofollow" title="تطبيق الشات" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+
+\
+
+<a id="asim7"rel="dofollow" title="اختصارات الشات" href=""style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+
+\
+
+</div></div><center>`, `<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="asim12">سهر الشام</font> ● · آ ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp;قريبا &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;قريبا&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp;قريبا&nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; قريبا &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; قريبا&nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; قريبا&nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات ●  &nbsp;
+
+\
+
+</marquee></div><center>`, 'body', `<link rel="stylesheet" href="https://raw.githack.com/alshanfara/shk1/main/alsh.css">
+
+\
+
+`, 'label.nosel.ninr.fl.uzr.label.label-primary', ` <center><div style=" width: 99%;height: auto!important; " id="sa" margin:1px;" class="fa fa-heartbeat btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+
+\
+
+‏	<font color="#fff">&nbsp; •( إضغط هنا القرآن الكريم)• &#9679; &nbsp;</font></div><div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary"></i></p>\
+
+‏	<b><font color="#000000" size="5">اريحوا مسامعكم </font></b></p><audio controls loop="" play=""><source src="https://h.top4top.io/m_1595scph70.mp3"></audio></div><center>`, `<a id="des22" style="padding: 2px 0px!important;border-radius:1px 1px 1px 1px;font-size: 12px !important; -webkit-transform: matrix(1,0,0,1,0,-135)!important;border: 1px solid white;width:100px;float:right;font-family: 'jazeera',FontAwesome;margin:-65px 2px 2px;transform:matrix(1, 0, 0, 1, 4, 24);" class="label label label-primary " target="_blank" href="https://qamar0.net/">شات قمر العراق</a>
+
+\
+
+`, `<style>
+
+\
+
+#alshanfara {
+
+\
+
+       background: #cc4266!important;
+
+\
+
+\
+
+width:100%!important;
+
+\
+
+    font-family: 'jazeera',FontAwesome;
+
+\
+
+color:#fff!important;
+
+\
+
+}
+
+\
+
+.alshanfara1{
+
+\
+
+    background-color: #0a8ebc!important;
+
+\
+
+    color: #fff!important;
+
+\
+
+}
+
+\
+
+</style>`];
