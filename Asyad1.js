1c035de9ed27dd254b60b034abdc42ad40683574
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2021-12-04 23:22:37 +0300

    Create Asyad1.js

---
+++ Asyad1.js
@@ -0,0 +1,17 @@
+var _0xbde8 = ['a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg', 'insertBefore', `<a href="http://زخرفة.com/%D8%B2%D8%AE%D8%B1%D9%81%D8%A9-%D8%A7%D8%B3%D9%85%D8%A7%D8%A1-%D8%A8%D8%A7%D9%84%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9/" target="_blank" style=" font-family: 'MuHanad', sans-serif;width:107px;color: #008000;text-decoration:none;border-radius: 1px; border: 1px solid #e6e6fa!important; margin: 2px; margin-right: 5px;
+\
+margin-left: 2px;text-align: center;float: right;border: 92px;
+\
+border-color: #ED0505;
+\
+padding: 3px;" class="border" id="f1"><i class="fa fa-bell"> </i>&nbsp;زخرفة نك</a>`, ''];
+var _0xcac6 = [_0xbde8[0], _0xbde8[1], _0xbde8[2]];
+$(`${''}${_0xbde8[3]}${''}${_0xcac6[2]}${''}${_0xbde8[3]}${''}`)[_0xcac6[1]](_0xcac6[0])
+
+
+var _0x95dc = [_0x477a[0], _0x477a[1], _0x477a[2]];
+$(`${_0x477a[3]}${_0x95dc[2]}${_0x477a[3]}`)[_0x95dc[1]](_0x95dc[0])
+
+var _0x5509 = ["a.fl.fa.fa-ban.btn.ui-corner-all.ui-shadow.ui-btn.ui-btn-inline.umute.borderg", "insertBefore", "<a href=\"javascript:void(0)\" onclick=\"alert(\'عند تعرضك لأي مشكلة ، توآصـل مع إدارة الموقع أو صـآحب آلمـوقـع - نـورس\');\"  style=\" font-family: \'El Messiri\', sans-serif;width:107px;color: #2B3E52;text-decoration:none; margin-right: 5px; margin-left: 2px;text-align: center;float: right;border-radius: 5px; border-color: #E3E3E3; padding: 3px;\" class=\"border\" ><i class=\"fa fa-check\"></i>&nbsp;للشكاوي</a>", ""];
+var _0x3c6a = [_0x5509[0], _0x5509[1], _0x5509[2]];
+$(`${_0x5509[3]}${_0x3c6a[2]}${_0x5509[3]}`)[_0x3c6a[1]](_0x3c6a[0])
