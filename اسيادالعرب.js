80c48b1a1995259bae009f3c6065626194775346
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2021-12-09 23:13:43 +0300

    Create اسيادالعرب.js

---
+++ اسيادالعرب.js
@@ -0,0 +1,21 @@
+$(`<link rel="stylesheet" href="https://cdn.statically.io/gh/mmnm399819/dimask98/master/%D8%A3%D8%B3%D9%8A%D8%A7%D8%AF%D8%A7%D9%84%D8%B9%D8%B1%D8%A8.css">`).insertBefore('body');
+$.getScript('https://cdn.statically.io/gh/mmnm399819/dimask98/master/Asyadidara.js');
+$(`<center><div id="laith1"><a id="laith7"rel="dofollow" title="القوانين" href=""style="border-radius: 0 4px 0 0; border-right: 2px solid#f7d322;"class="fl mini  fa fa-bullhorn btn btn-primary" target="_blank"></a>
+<a id="laith7"rel="dofollow" title="الاشتراكات" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-star-half-o btn btn-primary" target="_blank"></a>
+<a id="laith7"rel="dofollow" title="راديو" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-volume-up btn btn-primary" target="_blank"></a>
+<a id="laith7"rel="dofollow" title="زخرفة اسماء" href=""style="border-right: 1px solid#fff;"class="fl mini  fa fa-fire btn btn-primary" target="_blank"></a><a id="laith7"rel="dofollow" title="اتصل بنا" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-envelope btn btn-primary" target="_blank"></a>
+<a id="laith7"rel="dofollow" title="قوانين السوابر" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-exclamation-triangle btn btn-primary" target="_blank"></a>
+<a id="laith7"rel="dofollow" title="تطبيق الشات" href=""style="border-right: 1px solid#fff;"class="fl mini fa fa-android btn btn-primary" target="_blank"></a>
+<a id="laith7"rel="dofollow" title="اختصارات الشات" href=""style="border-radius: 6px 0 0 0; border-left: 3px solid#f7d322; border-right: 1px solid#ffffff;"class="fl mini fa fa-tencent-weibo btn btn-primary" target="_blank"></a>
+</div></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`<center><div><marquee direction="right" id="as43" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="3"> &nbsp;أهلا وسهلا بكم في شات <font color="#595757" class="asim12">اسياد العرب</font> ● · آ ‫أفضل سوبر لهذا الأسبوع · ● <font class="asim11" color="#727A54">&nbsp;قريبا &nbsp;</font> ● · ‫أفضل زائر لهذا الأسبوع · ● <font class="asim11" color="#ff9900">&nbsp;قريبا&nbsp;</font> ● · ‫أفضل زائرة لهذا الأسبوع · ● <font class="asim11" color="#ff00ff">&nbsp;قريبا&nbsp;</font> ● · مبدع الحائط لهذا الأسبوع · ● <font class="asim11" color="#5CAD9A">&nbsp; قريبا &nbsp;</font> ● · مبدعة الحائط لهذا الأسبوع · ● <font class="asim11" color="#800000">&nbsp; قريبا&nbsp;</font> ● · ‫أفضل تواجد لهذا الأسبوع · ● <font class="asim11" color="#417570">&nbsp; قريبا&nbsp;</font> ● · مع تمنياتنا للجميع بـ أطيب الاوقات ●  &nbsp;
+\
+</marquee></div><center>`).insertBefore('div#tlogins .lonline.light.break');
+$(`
+<style>
+.light { background: repeating-linear-gradient(45deg,rgba(255,255,255,.8),rgb(255,255,255) 100px,#ffffffb0 0,#fff6f699 200px),repeating-linear-gradient(-45deg,rgba(255,255,255,.5),rgba(255,255,255,.93) 100px,#b3adadd1 0,#ffffffe6 200px)!important}
+.fl.u-pic{ height:42px!important; width:42px!important;}
+
+ 
+
+ </style>`).insertBefore('body');
