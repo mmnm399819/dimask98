7286103b9fa1655bdb1afaed5319ab0c056e62a1
mmnm399819 <57255332+mmnm399819@users.noreply.github.com>
2021-12-09 19:43:21 +0300

    Create Hukuk.js

---
+++ Hukuk.js
@@ -0,0 +1,1 @@
+$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>صاحب الموقع : <a href='https://api.whatsapp.com/send?phone=966553491881&text=&source=&data=/'> مهند السوري </a></div>").insertAfter($(".loginstat")); },1000);
